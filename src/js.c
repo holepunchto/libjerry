c72e73acc89a2fd3350652226e498bc1a47876ca
Kasper Isager Dalsgarð <kasperisager@hey.com>
2026-06-29 12:08:28 +0200

    Add `js_create_object_with_properties()`

--- src/js.c
+++ src/js.c
@@ -2749,6 +2749,27 @@ js_create_object_with_prototype(js_env_t *env, js_value_t *prototype, js_value_t
   return 0;
 }
 
+int
+js_create_object_with_properties(js_env_t *env, js_value_t *prototype, js_value_t *const property_names[], js_value_t *const property_values[], size_t property_count, js_value_t **result) {
+  // Allow continuing even with a pending exception
+
+  js__enter(env);
+
+  jerry_value_t object = jerry_object();
+
+  jerry_value_free(jerry_object_set_proto(object, js__value_from_abi(prototype)));
+
+  for (size_t i = 0; i < property_count; i++) {
+    jerry_value_free(jerry_object_set(object, js__value_from_abi(property_names[i]), js__value_from_abi(property_values[i])));
+  }
+
+  *result = js__value_to_abi(object);
+
+  js__attach_to_handle_scope(env, env->scope, *result);
+
+  return 0;
+}
+
 static void
 js__on_function_finalize(void *data, jerry_object_native_info_t *info) {
   free(data);
